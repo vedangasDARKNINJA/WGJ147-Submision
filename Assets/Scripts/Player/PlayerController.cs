1a78ab906616267e1ec8b090bd80cfb93d46e1b9
vedangJavdekar <vedang.javdekar@gmail.com>
2020-05-04 20:02:09 +0530

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+
+    float input_h;
+
+
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input_h = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Animate();
+    }
+
+    void Move()
+    {
+        Vector2 vel = rb.velocity;
+        vel.x = velocity * input_h;
+        rb.velocity = vel;
+    }
+
+    void Animate()
+    {
+        if(input_h<0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(input_h>0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        anim.SetFloat("Velocity_X", Mathf.Abs(input_h));
+        anim.SetFloat("Velocity_Y", rb.velocity.y);
+    }
+}
---
+++ Assets/Scripts/World/SetBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SetBackground : MonoBehaviour
+{
+    public CompositeCollider2D world;
+
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (world != null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.drawMode = SpriteDrawMode.Tiled;
+            Bounds worldBounds = world.bounds;
+            Vector3 pos = world.transform.position + worldBounds.center;
+            spriteRenderer.size = worldBounds.size;
+            transform.position = pos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
